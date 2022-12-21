5d4ece45813e8e70b5926176d7e7c4cdf4418b08
Matt-Shenkman <shenkman@seas.upenn.edu>
2022-12-21 13:07:16 -0500

    project start

---
+++ src/pages/Home.js
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import { useEffect } from 'react';
+import { Tabs, Tab } from '@material-ui/core';
+
+function HomePage() {
+    
+  return (
+    <div>
+    <div>
+      <Tab label="Home" />
+      <Tab label="About" />
+      <Tab label="Contact" />
+    </div>
+        <div class = "body">
+            
+        </div>
+    </div>
+    
+    );
+
+}
+
+export default HomePage;
